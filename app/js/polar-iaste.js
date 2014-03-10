a038bbe0a3514799bff6b50a81d648f1ff1840f9
Gabriel <gabriel.sprenger@gmail.com>
2014-03-10 18:22:33 +0100

    Added random chart generation with different angles for testing purposes

--- app/js/polar-iaste.js
+++ app/js/polar-iaste.js
@@ -1,17 +1,34 @@
-var chartData = [], amount;
+// Random chart generation
+var randomChart = [], amount;
 amount = +(Math.random()*10).toFixed()+1;
+angles = (function() {
+  var tab = [],
+      sum = 0, 
+      total = 360;
+  for (var i=0; i<amount; i++) {
+    tab[i] = +(Math.random()*100).toFixed();
+  }
+  for (var i=0; i<amount; i++) {
+    sum += tab[i];
+  }
+  for (var i=0; i<amount; i++) {
+    tab[i] = (tab[i]/sum)*total;
+  }
+  return tab;
+})();
 for (var i=0; i<amount; i++) {
-  chartData.push({
+  randomChart.push({
     min: 0,
     value: +(Math.random()*100).toFixed(),
     max: 100,
-    angle: 2*Math.PI/amount,
+    angle: angles[i],
     unit: "kg",
     color: '#'+Math.floor(Math.random()*16777215).toString(16)
   });
 }
 
 $(document).ready(function() {
+  var chartData = randomChart;
   var myPolarArea = new Chart(document.getElementById("canvas").getContext("2d")).PolarArea(chartData, {
     scaleShowLabels: false,
     scaleShowLine: false
