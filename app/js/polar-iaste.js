e0e06d5db6151daa0a639d876950b1493298bd0c
Gabriel <gabriel.sprenger@gmail.com>
2014-03-10 17:09:27 +0100

    Created app structure and added JQuery and Bootstrap

---
+++ app/js/polar-iaste.js
@@ -0,0 +1,20 @@
+var chartData = [], amount;
+amount = +(Math.random()*10).toFixed();
+for (var i=-1; i<amount; i++) {
+  chartData.push({
+    min: 0,
+    value: +(Math.random()*100).toFixed(),
+    max: 100,
+    angle: 2*Math.PI/amount,
+    unit: "kg",
+    color: '#'+Math.floor(Math.random()*16777215).toString(16)
+  });
+}
+
+$(document).ready(function() {
+  var myPolarArea = new Chart(document.getElementById("canvas").getContext("2d")).PolarArea(chartData, {
+    scaleShowLabels: false,
+    scaleShowLine: false
+  });
+  $('pre').text(JSON.stringify(chartData, null, '\t'));
+});
